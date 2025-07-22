d272df0ec8f5dc6a76fa1598e9601b267edeeebf
rfetyan94 <rfetyan@seas.upenn.edu>
2025-07-22 16:49:22 -0600

    Update AMM.t.sol

--- AMM.t.sol
+++ AMM.t.sol
@@ -27,14 +27,12 @@ contract AMMTest is Test {
 	event Withdrawal( address indexed _from, address indexed recipient, uint256 AQty, uint256 BQty );
 
     function setUp() public {
-
 		uint256 amtA = 2**50;
 		uint256 amtB = 2**50;
-	
+		
 		vm.startPrank(admin);
 		tokenA = new MToken( 'Allosaurus', 'ALRS', 4*amtA);
 		tokenB = new MToken( 'Baryonyx', 'BYNX', 4*amtB);
-
 		tokenA.transfer(lp,amtA);
 		tokenB.transfer(lp,amtB);
 		vm.stopPrank();
@@ -51,19 +49,16 @@ contract AMMTest is Test {
 
 		assertEq( tokenA.balanceOf(address(amm)), amtA );
 		assertEq( tokenB.balanceOf(address(amm)), amtB );
-
     }
 
     function testSwap(address userA, address userB, uint256 ratioA, uint256 ratioB ) public {
-		vm.assume( ratioA > 4*10**6 ); //AMM takes a fee of 3bps, so we can't trade less than .3% of the current balance.  Assume a denominator of 10**9, so 4*10**6 is .4%
+		vm.assume( ratioA > 4*10**6 );
 		vm.assume( ratioB > 4*10**6 );
-		//vm.assume( ratioA < 2**40 ); //These fail with error "Too many rejections" -- it seems like foundry is using rejection sampling
-		//vm.assume( ratioB < 2**40 );
 		vm.assume( userA != address(0) );
 		vm.assume( userB != address(0) );
 		vm.assume( userA != userB );
 
-		uint256 amtA = Math.min(tokenA.balanceOf(address(amm))*(ratioA % 2**100)/10**9,tokenA.balanceOf(admin)); //Transfer amount can't be more than the balance of admin.  If we don't mod out ratioA, we get an arithmetic overflow when ratioA is close to 2**256
+		uint256 amtA = Math.min(tokenA.balanceOf(address(amm))*(ratioA % 2**100)/10**9,tokenA.balanceOf(admin));
 		uint256 amtB = Math.min(tokenB.balanceOf(address(amm))*(ratioB % 2**100)/10**9,tokenB.balanceOf(admin));
 		
 		vm.startPrank(admin);
@@ -81,8 +76,12 @@ contract AMMTest is Test {
 		amm.tradeTokens( address(tokenA), amtA );
 		vm.stopPrank();
 
-		assertEq( tokenA.balanceOf(userA), prevBalA - amtA );
-		assertGt( tokenB.balanceOf(userA), prevBalB ); //Change this to actually calculate amount
+		uint256 balInBefore = tokenA.balanceOf(address(amm));
+		uint256 balOutBefore = tokenB.balanceOf(address(amm));
+		uint256 amountInWithFee = amtA * 9997 / 10000;
+		uint256 expectedOut = balOutBefore - (balInBefore * balOutBefore) / (balInBefore + amountInWithFee);
+		//Change this to actually calculate amount
+		assertEq(tokenB.balanceOf(userA), prevBalB + expectedOut);
 
 		prevBalA = tokenA.balanceOf(userB);
 		prevBalB = tokenB.balanceOf(userB);
@@ -93,19 +92,20 @@ contract AMMTest is Test {
 		amm.tradeTokens( address(tokenB), amtB );
 		vm.stopPrank();
 
-		assertEq( tokenB.balanceOf(userB), prevBalB - amtB );
-		assertGt( tokenA.balanceOf(userB), prevBalA ); //Change this to actually calculate amount
-
+		balInBefore = tokenB.balanceOf(address(amm));
+		balOutBefore = tokenA.balanceOf(address(amm));
+		amountInWithFee = amtB * 9997 / 10000;
+		expectedOut = balOutBefore - (balInBefore * balOutBefore) / (balInBefore + amountInWithFee);
+		//Change this to actually calculate amount
+		assertEq(tokenA.balanceOf(userB), prevBalA + expectedOut);
     }
 
     function testWithdrawalA( address recipient, uint256 amtA ) public {
 		vm.assume( amtA > 0 );
 		vm.assume( recipient != address(0) );
-
 		uint256 amt = Math.min(tokenA.balanceOf(address(amm)),amtA); 
 		uint256 prevBalRecipient = tokenA.balanceOf(recipient);
 		uint256 prevBalAMM = tokenA.balanceOf(address(amm));
-
 		vm.expectEmit(true, true, false, true );
 		emit Withdrawal( lp, recipient, amt, 0 );
 		vm.prank(lp);
@@ -118,9 +118,7 @@ contract AMMTest is Test {
 		vm.assume( amtA > 0 );
 		vm.assume(withdrawer != lp );
 		vm.assume( recipient != address(0) );
-
 		uint256 amt = Math.min(tokenA.balanceOf(address(amm)),amtA); 
-
 		vm.expectRevert();
 		vm.prank(withdrawer);
 		amm.withdrawLiquidity(recipient,amt,0);
@@ -133,15 +131,12 @@ contract AMMTest is Test {
 		vm.assume(withdrawer != address(0));
 		vm.assume( recipient != address(0) );
 		vm.assume( recipient != withdrawer );
-
 		uint256 amtA = Math.min(tokenA.balanceOf(admin),_amtA); 
 		uint256 amtB = Math.min(tokenB.balanceOf(admin),_amtB); 
-
 		vm.startPrank(admin);
 		tokenA.transfer(withdrawer,amtA);
 		tokenB.transfer(withdrawer,amtB);
 		vm.stopPrank();
-
 		vm.startPrank(withdrawer);
 		tokenA.approve(address(amm),amtA);
 		tokenB.approve(address(amm),amtB);
@@ -150,6 +145,5 @@ contract AMMTest is Test {
 		amm.withdrawLiquidity(recipient,amtA,0);
 		vm.stopPrank();
     }
-
 
 }

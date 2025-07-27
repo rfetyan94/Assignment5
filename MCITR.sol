adb0305fa3ffc838259400b1af24b09fadda986b
rfetyan94 <rfetyan@seas.upenn.edu>
2025-07-27 11:49:55 -0600

    Update MCITR.sol

--- MCITR.sol
+++ MCITR.sol
@@ -1,18 +1,24 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/*
+  A basic ERC777 contract with access-controlled minting
+*/
 contract MCITR is ERC777, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-	constructor(uint256 initialSupply, address admin ) ERC777("MCIT Re-entry", "MCITR", new address[](0) ) {
+    constructor(uint256 initialSupply, address admin)
+        ERC777("MCIT Reentrancy", "MCITR", new address )
+    {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _grantRole(MINTER_ROLE, msg.sender );
-		_mint(msg.sender, initialSupply, "", "");
-	}
+        _grantRole(MINTER_ROLE, admin);
+        _mint(admin, initialSupply, "", "");
+    }
 
-	function mint(address to, uint256 amount ) public onlyRole(MINTER_ROLE) {
-		_mint( to, amount, "", "" );	
-	}
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount, "", "");
+    }
 }
